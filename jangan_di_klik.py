056ee4a88f8e9b05d0d85af8454f941f3d486094
Watashi Giorno Giovanna <118345808+muhamadhafis@users.noreply.github.com>
2023-04-29 12:05:39 +0700

    upload file jangan_di_klik.py

---
+++ jangan_di_klik.py
@@ -0,0 +1,10 @@
+import time
+import webbrowser
+
+def open_browser():
+    browser = webbrowser.get()
+    for _ in range(15):
+        browser.open_new("https://www.google.com/search?q=kamu+ngapain+diklik+++++++++++++++++++++++++++++++++++++++++++++++tapi+tenang+ini+akan+buka+sebanyak+10x+aja+kok&newwindow=1&sxsrf=APwXEdeh-dh0q9BiaWyH6ztv9wm2JKX_Gw%3A1682744397054&ei=TaRMZNH5AtGD4-EP1s6u8Ak&ved=0ahUKEwiR1KKwp87-AhXRwTgGHVanC54Q4dUDCA8&oq=kamu+ngapain+diklik+++++++++++++++++++++++++++++++++++++++++++++++tapi+tenang+ini+akan+buka+sebanyak+15x+aja+kok&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQDEoECEEYAFAAWABgAGgAcAF4AIABAIgBAJIBAJgBAKABAQ&sclient=gws-wiz-serp")
+        time.sleep(1 * 2.5)
+
+open_browser()
